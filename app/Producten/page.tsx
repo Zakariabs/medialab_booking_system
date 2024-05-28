b2e3cce3a13f6475a1ba75c081f6106d3e39bc18
Selma <145977452+SelmaElMahyaoui@users.noreply.github.com>
2024-05-28 10:57:17 +0200

    installed the dependencies needed

---
+++ app/Producten/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React from "react";
+import { Card } from "@/components/ui/card";
+import UseProducts from "../UseProducts/UseProducts";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+declare module 'react'; // Add the missing type declaration for React
+
+export default function Page() {
+  const products = UseProducts();
+
+  console.log(products)
+
+  return (
+    <div className="flex flex-wrap justify-center items-center gap-10 mt-28">
+      {products &&
+        products.products.map((product) => (
+          <div key={product.id}>
+            <Card className="max-w-sm rounded-lg border-2 border-red-500 p-4">
+              <img
+                alt="Handycam CX405 camcorder"
+                className="mx-auto"
+                height="150"
+                src={product.images[0]}
+                style={{
+                  aspectRatio: "200/150",
+                  objectFit: "cover",
+                }}
+                width="200"
+              />
+              <div className="mt-4 text-center">
+                <p className="font-bold">{product.title} -</p>
+                <p className="text-sm">camcorder met Exmor R®</p>
+                <Link href={`/product/${product.id}`}>
+                  <Button className="mt-2" variant="destructive">
+                    Select
+                  </Button>
+                </Link>
+              </div>
+            </Card>
+          </div>
+        ))}
+    </div>
+  );
+}
