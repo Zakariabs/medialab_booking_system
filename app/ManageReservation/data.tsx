4d5cab509a5dfda07dbb78aa96f3a1c9e3076188
Selma <145977452+SelmaElMahyaoui@users.noreply.github.com>
2024-05-28 15:29:24 +0200

    reservatie info (dummy info)

---
+++ app/ManageReservation/data.tsx
@@ -0,0 +1,64 @@
+//data toevoegen voor de reservaties 
+
+let data=[
+
+    {
+        "Porduct_id":"1244",
+        "Reservation_Number":"23234",
+        "Date":"1/2/2022",
+        
+    },
+    
+    {
+        "Porduct_id":"1244",
+        "Reservation_Number":"23234",
+        "Date":"1/2/2022"
+    },
+    
+    {
+        "Porduct_id":"1244",
+        "Reservation_Number":"23234",
+        "Date":"1/2/2022"
+    },
+    
+    {
+        "Porduct_id":"1244",
+        "Reservation_Number":"23234",
+        "Date":"1/2/2022"
+    },
+    
+    {
+        "Porduct_id":"1244",
+        "Reservation_Number":"23234",
+        "Date":"1/2/2022"
+    },
+    
+    {
+        "Porduct_id":"1244",
+        "Reservation_Number":"23234",
+        "Date":"1/2/2022"
+    },
+    
+    {
+        "Porduct_id":"1244",
+        "Reservation_Number":"23234",
+        "Date":"1/2/2022"
+    },
+     
+    {
+        "Porduct_id":"1244",
+        "Reservation_Number":"23234",
+        "Date":"1/2/2022"
+    },
+     
+    {
+        "Porduct_id":"1244",
+        "Reservation_Number":"23234",
+        "Date":"1/2/2022"
+    },
+
+]
+//data van onze voobeeld figma prototype
+
+
+export default data
