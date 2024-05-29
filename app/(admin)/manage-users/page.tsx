b741213e936998e7c311b55303a7d96683d8af11
Selma <145977452+SelmaElMahyaoui@users.noreply.github.com>
2024-05-29 14:42:26 +0200

    made the imports and made the export function for the page

---
+++ app/(admin)/manage-users/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import data from './data';
+
+export default function Page() {
+    let label =["User ID","Email","Role","#Items, Info "];
+
+    
+}
