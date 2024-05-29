9e873f12e5a3b69882ea63430646dc8572dd7ee5
Selma <145977452+SelmaElMahyaoui@users.noreply.github.com>
2024-05-29 13:33:27 +0200

    voorbeeld data voor user management page (added 'role' to make sure admin knows who the user is)

---
+++ app/(admin)/manage-users/data.ts
@@ -0,0 +1,44 @@
+let data= [
+    {
+        id: 12345,
+        email: "stephanie@student.ehb.be",
+        role: "student",
+        items: 2,
+    },
+    {
+        id: 12346,
+        email: "selma@student.ehb.be",
+        role: "student",
+        items: 1,
+    },
+    {
+        id: 12347,
+        email: "raphaël@student.ehb.be",
+        role: "student",
+        items: 4,
+    },
+    {
+        id: 12348,
+        email: "ruben.dejonkheere@docent.ehb.be",
+        role: "docent",
+        items: 7,
+    },
+    {
+        id: 12349,
+        email: "mohamed@student.ehb.be",
+        role: "student",
+        items: 2,
+    },
+    {
+        id: 12345,
+        email: "stephanie@student.ehb.be",
+        role: "student",
+        items: 2,
+    },
+    {
+        id: 12346,
+        email: "selma@student.ehb.be",
+        role: "student",
+        items: 1,
+    },
+]
