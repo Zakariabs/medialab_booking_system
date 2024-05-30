f47c47e9abea7ece6aefff775bff6ceb02f7e8cb
Selma <145977452+SelmaElMahyaoui@users.noreply.github.com>
2024-05-30 14:11:11 +0200

    Made the home page/dashboard of the user main components

--- app/(user)/Home-page-user/page.tsx
+++ app/(user)/Home-page-user/page.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import Link from 'next/link';
+import Link from "next/link";
+import React from "react";
 
-export default function MaterialPage() {
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/Inventory" >
-                            Inventory
+export default function Home() {
+  const data = [
+    { label: "Products", link: "/Inventory" },
+    { label: "Return", link: "/manage-users" }, //change this to right path
+    { label: "Contact Us", link: "/manage-products" }, //change this to right path
+  ];
+return (
+    <>
+        <div className="felx justify-center">
+            <div className="bg-[url('/bg.svg')] -mt-10 bg-no-repeat w-full h-[222px] flex items-center justify-center">
+                <div className="flex  font-medium flex-col justify-center text-white items-center ">
+                    <text>Welcome</text>
+                    <text className="text-xl font-italic bg-cyan-500">User Name Here</text>
+                </div>
+            </div>
+
+            <div className="flex justify-around mt-10 flex-col gap-10">
+                {data.map((elm, i) => (
+                    <div
+                        key={i}
+                        className="p-5 rounded-lg cursor-pointer text-center font-semibold bg-cyan-500" // Adjust the color here
+                    >
+                        <Link href={elm.link}>
+                            <span className="text-white">{elm.label}</span>
                         </Link>
-                    </li>
-                </ul>
-            </nav>
-            <h1>Material Page</h1>
+                    </div>
+                ))}
+            </div>
         </div>
-    );
+    </>
+);
 }
