b82b278925098707475795240b8a508fe9dedb4a
Selma <145977452+SelmaElMahyaoui@users.noreply.github.com>
2024-05-30 14:09:35 +0200

    changed the file name and place to the HomeAdmin page

--- app/(admin)/HomeAdmin.tsx
+++ app/(admin)/HomeAdmin.tsx
@@ -1,5 +1,6 @@
-import Link from "next/link";
-import React from "react";
+"use client"
+import Link from "next/link"
+import React from "react"
 export default function Header() {
   let data = [
     { label: "Manage reservation", link: "/manage-reservation" },
