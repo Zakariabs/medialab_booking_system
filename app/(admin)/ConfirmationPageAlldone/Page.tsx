c9e31679a544e7bf310bc6aec11690245098b372
stephyeni <Stephanieeniola@gmail.com>
2024-06-03 10:01:03 +0200

    CHANGES HAS BEEN MADE

--- app/(admin)/ConfirmationPageAlldone/Page.tsx
+++ app/(admin)/ConfirmationPageAlldone/Page.tsx
@@ -1,49 +1,61 @@
+
+
+
 import type { NextPage } from "next";
 import { useCallback } from "react";
-import styles from "./confirmed-page.module.css";
+import { useRouter } from "next/router";
 
-const ConfirmedPage: NextPage = () => {
+const AllDone: NextPage = () => {
+  const router = useRouter();
+
   const onReturnButtonImageClick = useCallback(() => {
-    // Please sync "ManageReservationsPage" to the project
+    // Please sync "ClientHomeScreen" to the project
   }, []);
 
   const onClickHereTextClick = useCallback(() => {
-    // Please sync "ManageReservationsPage" to the project
-  }, []);
+    router.push("/all-done1");
+  }, [router]);
 
   return (
-    <div className={styles.confirmedpage}>
-      <b className={styles.allDone}>All Done</b>
+    <div className="w-full relative bg-neutral-white h-[852px] overflow-hidden text-justify text-smi-6 text-neutral-white font-poppins">
+      <b className="absolute top-[52px] left-[90px] text-21xl inline-block font-pridi text-mediumturquoise text-center w-[214px] h-[65px]">
+        All Done
+      </b>
       <img
-        className={styles.confirmedpageChild}
+        className="absolute top-[374px] left-[-149px] w-[692.4px] h-[626.3px] object-contain"
         alt=""
-        src="/ellipse-121.svg"
+        src="/ellipse-12.svg"
       />
-      <img className={styles.confirmedpageItem} alt="" src="/ellipse-131.svg" />
-      <div className={styles.theReservationHasContainer}>
-        <p className={styles.blankLine}>&nbsp;</p>
-        <p className={styles.blankLine}>
-          the reservation has been successfully accepted and the orderer will be
-          kept informed
-        </p>
+      <img
+        className="absolute top-[-27px] left-[-133px] w-[753.3px] h-[714px] object-contain"
+        alt=""
+        src="/ellipse-13.svg"
+      />
+      <div className="absolute top-[235px] left-[49px] font-black inline-block w-[305px] h-[95px]">
+        <p className="m-0">&nbsp;</p>
+        <p className="m-0">{`Your reservation has been well received and will be confirmed by one of our staff members as soon as possible. In case of any problems of the reservation, you will be kept informed via `}</p>
       </div>
-      <div className={styles.changeYourMind}>
-        change your mind and still wish to decline ?
+      <div className="absolute top-[400px] left-[59px] text-base font-black text-center inline-block w-[286px] h-[17px]">
+        Raphael.dufour@student.ehb.be
+      </div>
+      <div className="absolute top-[729px] left-[61px] font-black inline-block w-[282px] h-[26px]">
+        change your mind and still wish to cancel ?
       </div>
       <img
-        className={styles.returnbuttonIcon}
+        className="absolute top-[558px] left-[170px] w-[63.1px] h-[57px] object-contain cursor-pointer"
         alt=""
-        src="/returnbutton2@2x.png"
+        src="/returnbutton1@2x.png"
         onClick={onReturnButtonImageClick}
       />
-      <div className={styles.confirmedpageInner} />
-      <div className={styles.clickHere} onClick={onClickHereTextClick}>
+      <div className="absolute top-[757px] left-[143px] rounded-3xs bg-neutral-white w-[118px] h-[26px]" />
+      <div
+        className="absolute top-[761px] left-[169px] font-black text-mediumturquoise inline-block w-[77px] h-[22px] cursor-pointer"
+        onClick={onClickHereTextClick}
+      >
         Click here
       </div>
     </div>
   );
 };
 
-// All -done
-
-export default ConfirmedPage;
+export default AllDone;
