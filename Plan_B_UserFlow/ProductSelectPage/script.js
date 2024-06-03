38b2fe2cdf2b525dd1d991115fe2130c71995c48
Raphaelduf <raphael.dufour@student.ehb.be>
2024-06-03 16:30:36 +0200

    Plan_B_UserExperience

---
+++ Plan_B_UserFlow/ProductSelectPage/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.select-button').addEventListener('click', function() {
+    window.location.href = '../ProductSelectDate/index.html';
+});
+
+document.querySelector('.return-button').addEventListener('click', function() {
+    window.location.href = '../ReservateProducts/index.html';
+});
